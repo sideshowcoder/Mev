ed62bc7b337f3a998582f5759918952208f23504
Philipp Fehre <philipp.fehre@googlemail.com>
2011-05-10 16:30:20 +0200

    fixed some code style issues still buggy sorry some error in package.json

--- mev/bin/mev.js
+++ mev/bin/mev.js
@@ -1,122 +1,120 @@
 #!/usr/bin/env node
 
 // START WRAPPER
-;(function(){
-
-  /* Reverse DNS resolver script for command line use
- * Author: Philipp Fehre <philipp.fehre@googlemail.com>
- * 
- * Provide a way to parallely resolve a massive number of Reverse DNS Requests
- * Uses Redis to Store requests as well as results
- *
- * This is part of the iStrukta Project for internet analyzation
- */
-
-// Get libs for command line parsing and mev
-var nomnom = require('nomnom')
-  , nodrrr = require('nodrrr')
-	,	Mev = require('../lib/mev')
-  ,	Rdns = require('../lib/rdns')
-
-// 	I/O
-var	sys = require('sys')
-	,	fs = require('fs')
-
-// Specify opts
-var opts = [
-	{	
-		name: 'help'
-	, string: '-h'
-	,	help: 'Show help'
-	}
-,	{	
-		name: 'debug'
-	,	string: '-d'
-	,	help: 'Debug mode logging everything done'
-	}
-,	{
-		name: 'stats'
-	,	string: '--stat'
-	,	help: 'show status'
-	}
-,	{
-	  name: 'requests'
-	,	string: '-r PATH, --requests=PATH'
-	,	help: 'Requests to run'
-	}
-, {
-    name: 'module'
-  , string: '-m MODUL, --module=MODUL'
-  , help: 'specify module to use currently availible: rdns'
+(function(){
+  
+    /* Reverse DNS resolver script for command line use
+   * Author: Philipp Fehre <philipp.fehre@googlemail.com>
+   * 
+   * Provide a way to parallely resolve a massive number of Reverse DNS Requests
+   * Uses Redis to Store requests as well as results
+   *
+   * This is part of the iStrukta Project for internet analyzation
+   */
+  
+  // Get libs for command line parsing and mev
+  var nomnom = require('nomnom'),
+      nodrrr = require('nodrrr'),
+  		Mev = require('../lib/mev'),
+    	Rdns = require('../lib/rdns');
+  
+  // 	I/O
+  var	sys = require('sys'),
+  		fs = require('fs');
+  
+  // Specify opts
+  var opts = [
+  	{	
+  		name: 'help',
+  	  string: '-h',
+  		help: 'Show help'
+  	},
+  	{	
+  		name: 'debug',
+  		string: '-d',
+  		help: 'Debug mode logging everything done'
+  	},
+  	{
+  		name: 'stats',
+  		string: '--stat',
+  		help: 'show status'
+  	},
+  	{
+  	  name: 'requests',
+  		string: '-r PATH, --requests=PATH',
+  		help: 'Requests to run'
+  	},
+   {
+      name: 'module',
+      string: '-m MODUL, --module=MODUL',
+      help: 'specify module to use currently availible: rdns'
+    },
+   {
+      name: 'output',
+      string: '-o FILEOUT, --output=FILEOUT',
+      help: 'specify output to file instead of redis, with filename'
+    },
+   {
+      name: 'dbidx',
+      string: '--idx',
+      help: 'specifiy the redis index to use to save the results'
+    },
+   {
+      name: 'autoshutdown',
+      string: '--autoshutdown',
+      help: 'enable mev autoshutdown after last request, this is still really unstable!'
+    },
+   {
+      name: 'growl',
+      string: '--growl',
+      help: 'turn on growl support'
+    },
+   {
+      name: 'growlhost',
+      string: '--growlhost',
+      help: 'Host to sent growl notifications to defaults to localhost'
+    },
+   {
+      name: 'growlpass',
+      string: '--growlpass',
+      help: 'Host remote growl password defaults to empty'
+    }
+  ]
+  
+  // Parse opts
+  var options = nomnom.parseArgs(opts)
+  
+  
+  // Init
+  if (options.requests) {
+    if(options.growl){
+      // Growl specs
+      var host = options.growlhost || 'localhost',
+          app = 'mev',
+          all = ['Mev status'],
+          def = ['Mev status'],
+          pass = options.growlpass || null;
+      // Setup
+      var growl = new nodrrr.Nodrrr(host, app, all, def, pass);
+      // Register
+      growl.register();
+    }
+    // Load and run reverse dns
+    if(options.module == 'rdns') {
+      var module = new Rdns('rdns'),
+  	    	mev = new Mev(module, options.requests, options.debug, options.stats, options.output, options.dbidx, options.autoshutdown);
+      if(options.growl) growl.notify('Mev status', 'Mev RDNS resolver', 'RDNS resolver start', 0, false);
+      mev.start();
+    }
   }
-, {
-    name: 'output'
-  , string: '-o FILEOUT, --output=FILEOUT'
-  , help: 'specify output to file instead of redis, with filename'
-  }
-, {
-    name: 'dbidx'
-  , string: '--idx'
-  , help: 'specifiy the redis index to use to save the results'
-  }
-, {
-    name: 'autoshutdown'
-  , string: '--autoshutdown'
-  , help: 'enable mev autoshutdown after last request, this is still really unstable!'
-  }
-, {
-    name: 'growl'
-  , string: '--growl'
-  , help: 'turn on growl support'
-  }
-, {
-    name: 'growlhost'
-  , string: '--growlhost'
-  , help: 'Host to sent growl notifications to defaults to localhost'
-  }
-, {
-    name: 'growlpass'
-  , string: '--growlpass'
-  , help: 'Host remote growl password defaults to empty'
-  }
-
-]
-
-// Parse opts
-var options = nomnom.parseArgs(opts)
-
-
-// Init
-if (options.requests) {
-  if(options.growl){
-    // Growl specs
-    var host = options.growlhost || 'localhost'
-      , app = 'mev'
-      , all = ['Mev status']
-      , def = ['Mev status']
-      , pass = options.growlpass || null
-    // Setup
-    var growl = new nodrrr.Nodrrr(host, app, all, def, pass);
-    // Register
-    growl.register()
-  }
-  // Load and run reverse dns
-  if(options.module == 'rdns') {
-    var module = new Rdns('rdns')
-	    ,	mev = new Mev(module, options.requests, options.debug, options.stats, options.output, options.dbidx, options.autoshutdown)
-    if(options.growl) growl.notify('Mev status', 'Mev RDNS resolver', 'RDNS resolver start', 0, false)
-  ; mev.start()
-  }
-}
-
-mev.on('shutdown', function(){
-  // Done using auto shutdown
-  if(options.growl) growl.notify('Mev stop', 'Mev RDNS resolver', 'RDNS resolver done', 0, false)
-; process.exit(0);
-})
-
-// END WRAPPER
-})()
+  
+  mev.on('shutdown', function(){
+    // Done using auto shutdown
+    if(options.growl) growl.notify('Mev stop', 'Mev RDNS resolver', 'RDNS resolver done', 0, false);
+    process.exit(0);
+  })
+  
+})() // END WRAPPER
 
 
 
--- mev/lib/dummy.js
+++ mev/lib/dummy.js
@@ -13,50 +13,49 @@ var mev = require(__dirname + '/mev'),
 function Dummy(id) {
 		
 	// Setup
-  var done = false;
-	var self = this;
-	this._mev = null;
-	// Name the module to be addressed via Mev
-	this.id = id;
-  // console.log("Module Dummy initialized with id " + this.id);
+  var done = false,
+      that = this;
+
+	that._mev = null;
+	that.id = id;
 	
 	// Write the result somewhere or just throw back
 	var finishRes = function(result){
-		self.emit('done', result);
+		that.emit('done', result);
 	};
 	
 	// Read input from a file
-	this.readInput = function(indata){
-		var data = { 'data' : indata };
-		self.emit('data', data);
+	that.readInput = function(indata){
+		var data = { data : indata };
+		that.emit('data', data);
 	};
 	
 	// Generate requests from data 
-	this.genReq = function(data){
-		var request = { 'request' : data['data'] };
-		self.emit('request', request);
+	that.genReq = function(data){
+		var request = { request : data };
+		that.emit('request', request);
 	};
 	
 	// Run a given Request
-	this.runReq = function(req){
-		var result = { 'result' : req['request'] };
-		self.emit('result', result);
+	that.runReq = function(req){
+		var result = { result : req.request };
+		that.emit('result', result);
 	};
 	
 	// Handle the result returned form a request
-	this.handleRes = function(result){
+	that.handleRes = function(result){
 		if(done) {
       done = false;
-			finishRes(result['result']);
+			finishRes(result.result);
 		} else {
-			var data = { 'data' : result['result'] };
+			var data = { data: result.result };
       done = true;
-			self.emit('data', data);
+			that.emit('data', data);‚
 		}
 	};
 };
 
-// Extend EventEmitter
+// Extend EventEmitter‚
 Dummy.prototype = new EventEmitter;
 
 // Export
