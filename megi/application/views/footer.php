884ab1598096c6786b589ad1ace9c50d8c509220
Philipp Fehre <philipp.fehre@googlemail.com>
2011-05-27 15:29:03 +0200

    moved primary javascript framework over to dojo completly, needed for ajaxification

--- megi/application/views/footer.php
+++ megi/application/views/footer.php
@@ -7,7 +7,7 @@
   </footer>
 </div>
 
-<script src=<?php echo base_url()."public/js/libs/dojo.js";?>></script>
+<script src=<?php echo base_url()."public/js/libs/dojo/dojo.js";?>></script>
 <script src=<?php echo base_url()."public/js/script.js";?>></script>
 <!--[if lt IE 7 ]>
 <script src=<?php echo base_url()."public/js/libs/dd_belatedpng.js";?>></script>
